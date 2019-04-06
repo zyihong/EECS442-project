d1901d3990f89b95a1013e95e628379777359870
zyihong <zyihong@umich.edu>
2019-04-06 12:51:17 -0400

    prepare data

---
+++ prepare_data.py
@@ -0,0 +1,62 @@
+import nltk
+import pickle
+from collections import Counter
+from pycocotools.coco import COCO
+
+
+CAPTION_PATH = 'data/annotations/captions_train2014.json'
+VOCAB_PATH = './data/vocab.pkl'
+THRESHOLD = 4
+
+
+class Vocabulary(object):
+    def __init__(self):
+        self.word_to_idx = {}
+        self.idx_to_word = {}
+        self.index = 0
+
+    def add_word(self, word):
+        if word not in self.word_to_idx:
+            self.word_to_idx[word] = self.index
+            self.idx_to_word[self.index] = word
+            self.index += 1
+
+    def __len__(self):
+        return len(self.word_to_idx)
+
+    def __call__(self, word):
+        if word not in self.word_to_idx:
+            return self.word_to_idx['<unk>']
+        return self.word_to_idx[word]
+
+
+def build_vocab(json_path, threshold):
+    coco_annotation = COCO(json_path)
+    counter = Counter()
+    ids = coco_annotation.anns.keys()
+    for i, id in enumerate(ids):
+        caption = str(coco_annotation.anns[id]['caption'])
+
+        # TODO: please master Chao explain these two lines
+        tokens = nltk.tokenize.word_tokenize(caption.lower())
+        counter.update(tokens)
+
+    words = [word for word, cnt in counter.items() if cnt >= threshold]
+
+    vocab = Vocabulary()
+    vocab.add_word('<pad>')
+    vocab.add_word('<start>')
+    vocab.add_word('<end>')
+    vocab.add_word('<unk>')
+
+    for word in words:
+        vocab.add_word(word)
+    return vocab
+
+
+def prepare_data():
+    vocab = build_vocab(CAPTION_PATH, THRESHOLD)
+    with open(VOCAB_PATH, 'wb') as f:
+        pickle.dump(vocab, f)
+    print('Save word vocab to {}'.format(VOCAB_PATH))
+
--- train.py
+++ train.py
@@ -1,9 +1,12 @@
 from torchvision import datasets, models, transforms
+from prepare_data import prepare_data
 
 from model import *
 
 
 def main():
+    prepare_data()
+
     net = EncoderNet()
 
     vgg16 = models.vgg16(pretrained=True)
