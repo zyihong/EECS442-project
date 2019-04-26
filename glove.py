08b3b7177c547484c0aa8e29e2e0037f48104efb
Heavynest <1019700136@qq.com>
2019-04-25 20:16:15 -0400

    glvoe

---
+++ glove.py
@@ -0,0 +1,13 @@
+import numpy as np
+def gloveembedding():
+    dict = {}
+    embeddic = []
+    f = open("./data/glove.6B/glove.6B.200d.txt", 'r', encoding='UTF-8')
+    for i, line in enumerate(f):
+        splitLine = line.split()
+        word = splitLine[0]
+        embeddic.append(np.array([float(val) for val in splitLine[1:]]))
+        dict[word] = i
+    embeddic = np.array(embeddic)
+    print("Done.", len(dict), " words loaded!")
+    return embeddic, dict,
